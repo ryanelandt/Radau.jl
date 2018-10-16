558dba008e8e0655741f3f93bd6543fbf797a39c
Ryan Elandt <ryan.elandt@gmail.com>
2018-10-16 18:19:44 -0400

    added functions and tests for programatic butcher table creation

--- src/Radau.jl
+++ src/Radau.jl
@@ -3,15 +3,22 @@ module Radau
 using ForwardDiff
 using StaticArrays
 using LinearAlgebra
+using Polynomials: Poly, polyval, polyder, coeffs
+using PolynomialRoots: roots
+using GenericLinearAlgebra
 
 
+include("generate_butcher_table.jl")
 include("radau_struct.jl")
 include("radau_functions.jl")
 include("radau_utilities.jl")
 include("radau_solve.jl")
 
-
 export
+    # generate_butcher_table.jl
+    find_real_eigenvalue,
+    radau_butcher_table_plus,
+
     # radau_struct.jl
     RadauIntegrator,
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test
 using Radau
 
-
+include("test_generate_butcher_table.jl")
 include("test_exports.jl")
 include("basic_test.jl")
