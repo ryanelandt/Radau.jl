bf8476051ff7d3614c834c65d3794b3a618d01ea
Ryan Elandt <ryan.elandt@gmail.com>
2018-10-21 16:36:25 -0400

    added printout to test exports

--- test/test_exports.jl
+++ test/test_exports.jl
@@ -2,5 +2,6 @@
     # Ensure that every exported name is actually defined
     for name in names(Radau)
         @test isdefined(Radau, name)
+        !isdefined(Radau, name) && println("MISSING: ", name)
     end
 end
